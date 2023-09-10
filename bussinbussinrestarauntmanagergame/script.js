76daefbf65ed376da39ec6bf44bea6273ed2cf58
gamedoescool <hi_ishan@outlook.com>
2023-09-10 10:05:50 -0700

    i made it better

--- bussinbussinrestarauntmanagergame/script.js
+++ bussinbussinrestarauntmanagergame/script.js
@@ -1,13 +1,10 @@
-var usernamePasscombo = {'user':'pass','dharveen':'is gay','1':'2'};
-var user = '';
-var loggedIn = false;
+restaurant = store.findRestaurant(username);
 // this function is called when the 'log in' button is clicked
 function login_clicked(){
     document.getElementById('output').innerHTML = '';
     var password = document.getElementById('password').value;
     var username = document.getElementById('username').value;
     
-    restaurant = store.findRestaurant(username);
     if (restaurant == null) {
         document.getElementById('output').innerHTML = 'RESTAURANT NOT FOUND';
         return;
@@ -19,8 +16,7 @@ function login_clicked(){
     }
 
     document.getElementById('output').innerHTML = 'yoo it works';
-    user = username;
-    loggedIn = true;
+    
     //DO NOT DELETE IMPORTANT DO NOT DELETE
     //var x = document.getElementsByClassName('loginthing');
     //var link = document.getElementsByClassName('loginthing2');
